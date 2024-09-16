057bedd40556fe1d8a0b4dc0cc3c74423ce44e41
stefaniso <stefaniso@stefan-Inspiron-7472>
2024-09-16 11:42:52 -0600

    11:42 A.M.

---
+++ chapter3/src/skillbuilders/template.java
@@ -0,0 +1,32 @@
+/* 
+
+Program: template.java              Date:
+
+Purpose
+
+Author
+School
+Course
+
+*/
+
+package skillbuilders;
+
+public class template {
+
+	public static void main(String[] args) {
+		//  Declaration area:
+		String abc;
+		
+		// Preparing for user input
+		// (scanner text here)
+
+	}
+
+}
+
+/* Screen Dump
+
+(paste code output here)
+
+*/
