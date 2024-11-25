311b91514e5fb4d731368f178f2994b9cc2591a7
27283001 <27283001@L815WD220006.edu.cbe.ab.ca>
2024-11-25 11:50:48 -0700

    11:50 A.M.

--- GettingStarted/src/simplePhidgets/BlinkLED.java
+++ GettingStarted/src/simplePhidgets/BlinkLED.java
@@ -9,14 +9,14 @@ public class BlinkLED {
 	        //Create | Here you've created a DigitalOutput object for your LED. An object represents how you interact with your device. DigitalOutput is a class from the Phidgets library that's used to provide a voltage to things like LEDs.
         DigitalOutput redLED = new DigitalOutput();
 	        //Address | This tells your program where to find the device you want to work with. Your LED is connected to port 1 and your code reflects that. IsHubPortDevice must be set if you are not using a Smart Phidget (more on this later).
-        redLED.setHubPort(1);
+        redLED.setHubPort(4); // green LED is plugged into port 4, red is plugged into port 1.
         redLED.setIsHubPortDevice(true);
 	        //Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000 to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
         redLED.open(1000);
 	        //Use your Phidgets | Here is where you can have some fun and use your Phidgets! You can turn your LED on/off by setting the state to true/false. The sleep command keeps your LED on by letting 1000 milliseconds pass before turning the LED off.
-        while(true){
+        for(int i = 0; i < 3; ++i){
             redLED.setState(true);
-            Thread.sleep(1000);
+            Thread.sleep(2000);
             redLED.setState(false);
             Thread.sleep(1000);
         }
