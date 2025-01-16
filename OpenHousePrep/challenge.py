0d17e4660bb26a20f069e8b38168e7d11dc633ec
stefaniso <stefaniso@stefan-Inspiron-7472>
2025-01-16 10:59:00 -0700

    10:58 A.M.

---
+++ OpenHousePrep/challenge.py
@@ -0,0 +1,18 @@
+"""
+
+COMPUTER SCIENCE OPEN HOUSE PYTHON EXPERIMENT:
+
+1.  Modify this program to ask the user for their last name, and age. Print this information.
+
+2.  Modify the program to print the approximate year the user was born. You can do this by taking the current year (2025) and subtracting the user's age.
+
+"""
+
+# ask the user for their details, and save them using the assignment operator
+firstName = input("Please enter your first name: ")
+lastName = ""
+
+
+# print out the user's details.
+print("First Name: " + firstName)
+print()
